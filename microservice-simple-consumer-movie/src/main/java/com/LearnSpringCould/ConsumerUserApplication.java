e5f34603ebff75121062b874678a5c6644d4cd41
zhangmz <souldeathr@gmail.com>
2017-09-22 14:51:07 +0800

    整合端点监控。

---
+++ microservice-simple-consumer-movie/src/main/java/com/LearnSpringCould/ConsumerUserApplication.java
@@ -0,0 +1,24 @@
+package com.LearnSpringCould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ConsumerUserApplication {
+
+    // 注入一个对象
+    @Bean
+    public RestTemplate restTemplate() {
+	return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+	SpringApplication.run(ConsumerUserApplication.class, args);
+    }
+
+}
